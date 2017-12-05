c10dda2e8c7acb52128d8026c304f93576e95a5d
Amir <Amir@10.51.171.28>
2017-12-05 17:00:02 +0100

    Amir hat zurück beleidigt

--- src/my/lebensmitteldatenbank/test.java
+++ src/my/lebensmitteldatenbank/test.java
@@ -12,7 +12,7 @@ package my.lebensmitteldatenbank;
 public class test {
     public static void main (String[] args){
         System.out.println("Ihr kleinen picos");
-        System.out.println("test von amirs pc");
+        System.out.println("du sehr kleiner picos");
     }
     
 }
